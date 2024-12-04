a2759cdf3b9cfbd580039c7ae578b8b5927c35bc
Nathan Gavin <nathangavin987@gmail.com>
2024-12-05 11:15:46 +1300

    extended database server processing during installation to account for IPv6 addresses

--- plugins/Installation/FormDatabaseSetup.php
+++ plugins/Installation/FormDatabaseSetup.php
@@ -173,7 +173,28 @@ class FormDatabaseSetup extends QuickForm2
             'enable_ssl'    => false
         );
 
-        if (($portIndex = strpos($dbInfos['host'], '/')) !== false) {
+        if (strpos($dbInfos['host'], '[') !== false && strpos($dbInfos['host'], ']') !== false) {
+            // host contains opening and closing square brackets
+
+            preg_match_all('/\[(.*?)\]/', $dbInfos['host'], $matches);
+            $internal = $matches[1];
+            // filter_var requires valid IPv6 addresses to not be encased in brackets
+            if (count($internal) > 0 && filter_var($internal[0], FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) !== false) {
+                // contents between the square brackets are a valid IPv6 address
+
+                $components = explode("]", $dbInfos['host']);
+                if (count($components) > 1) {
+                    // there is content after the closing square bracket 
+                    preg_match('/^(:\d+)$/', $components[1], $portMatches);
+                    if (count($portMatches) > 0) {
+                        // content after is valid format for a port number
+                        $dbInfos['port'] = substr($portMatches[1], 1);
+                    }
+                }
+                // db connector requires IPv6 to be encased in square brackets
+                $dbInfos['host'] = "[" . $internal[0] . "]";
+            }
+        } else if (($portIndex = strpos($dbInfos['host'], '/')) !== false) {
             // unix_socket=/path/sock.n
             $dbInfos['port'] = substr($dbInfos['host'], $portIndex);
             $dbInfos['host'] = '';
