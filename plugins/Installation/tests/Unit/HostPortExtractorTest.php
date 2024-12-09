476324327cce49147c47d5c921703aee36fcbe43
Nathan Gavin <nathangavin987@gmail.com>
2024-12-09 14:49:30 +1300

    Update tests to use data providers

--- plugins/Installation/tests/Unit/HostPortExtractorTest.php
+++ plugins/Installation/tests/Unit/HostPortExtractorTest.php
@@ -6,97 +6,43 @@ use Piwik\Plugins\Installation\HostPortExtractor;
 
 class HostPortExtractorTest extends \PHPUnit\Framework\TestCase
 {
-    public function testStandardIpAddressNoPort()
-    {
-        $IP = '127.0.0.1';
-        $extractedIPandPort = HostPortExtractor::extract($IP);
-
-        $this->assertNull($extractedIPandPort);
+    public function validInputs() {
+        return [
+            ['127.0.0.1:3000', ['127.0.0.1', '3000']],
+            ['localhost:3000', ['localhost', '3000']],
+            ['/test/path/socket', ['', '/test/path/socket']],
+            ['[2001:db8:3333:4444:5555:6666:7777:8888]', ['[2001:db8:3333:4444:5555:6666:7777:8888]', '']],
+            ['[2001:db8:3333:4444:5555:6666:7777:8888]:3000', ['[2001:db8:3333:4444:5555:6666:7777:8888]', '3000']],
+            ['[2001::8888]', ['[2001::8888]', '']],
+        ];
     }
 
-    public function testStandardIpAddressWithPort()
+    public function invalidInputs()
     {
-        $IpWithPort = '127.0.0.1:3000';
-        $extractedIPandPort = HostPortExtractor::extract($IpWithPort);
-
-        $this->assertEquals($extractedIPandPort->host, '127.0.0.1');
-        $this->assertEquals($extractedIPandPort->port, '3000');
+        return [
+            ['127.0.0.1'],
+            ['localhost'],
+            ['2001::8888'],
+            ['[2001::db8::8888]'],
+            ['[200r::11zz]'],
+        ];
     }
 
-    public function testAddressNoPort()
-    {
-        $address = 'localhost';
-        $extractedIPandPort = HostPortExtractor::extract($address);
-
-        $this->assertNull($extractedIPandPort);
+    /**
+     * @dataProvider validInputs
+     */
+    public function testValidDbHosts($input, $expected) {
+        $extractedHostAndPort = HostPortExtractor::extract($input);
+        $this->assertEquals($extractedHostAndPort->host, $expected[0]);
+        $this->assertEquals($extractedHostAndPort->port, $expected[1]);
     }
 
-    public function testAddressWithPort()
+    /**
+     * @dataProvider invalidInputs
+     */
+    public function testInvalidDbHosts($input)
     {
-        $address = 'localhost:3000';
-        $extractedIPandPort = HostPortExtractor::extract($address);
-
-        $this->assertEquals($extractedIPandPort->host, 'localhost');
-        $this->assertEquals($extractedIPandPort->port, '3000');
-    }
-
-    public function testUnixSocket()
-    {
-        $unixSocket = '/test/path/socket';
-        $extractedUnixSocket = HostPortExtractor::extract($unixSocket);
-
-        $this->assertEquals($extractedUnixSocket->host, '');
-        $this->assertEquals($extractedUnixSocket->port, '/test/path/socket');
-    }
-
-    public function testIPv6NoPort()
-    {
-        $IPv6 = '[2001:db8:3333:4444:5555:6666:7777:8888]';
-        $extractedIP = HostPortExtractor::extract($IPv6);
-
-        $this->assertEquals($extractedIP->host, '[2001:db8:3333:4444:5555:6666:7777:8888]');
-        $this->assertEquals($extractedIP->port, '');
-    }
-
-    public function testIPv6WithPort()
-    {
-        $IPv6WithPort = '[2001:db8:3333:4444:5555:6666:7777:8888]:3000';
-        $extractedIPandPort = HostPortExtractor::extract($IPv6WithPort);
-
-        $this->assertEquals($extractedIPandPort->host, '[2001:db8:3333:4444:5555:6666:7777:8888]');
-        $this->assertEquals($extractedIPandPort->port, '3000');
-    }
-
-    public function testIpv6Short()
-    {
-        $IPv6Short = '[2001::8888]';
-        $extractedIP = HostPortExtractor::extract($IPv6Short);
-
-        $this->assertEquals($extractedIP->host, '[2001::8888]');
-        $this->assertEquals($extractedIP->port, '');
-    }
-
-    public function testIPv6NoBrackets()
-    {
-        $IpNoBrackets = '2001::8888';
-        $extractedIP = HostPortExtractor::extract($IpNoBrackets);
-
-        $this->assertNull($extractedIP);
-    }
-
-    public function testIPv6TooManyColons()
-    {
-        $IPv6TooManyColons = '[2001::db8::8888]';
-        $extractedIP = HostPortExtractor::extract($IPv6TooManyColons);
-
-        $this->assertNull($extractedIP);
-    }
-
-    public function testIPv6InvalidChars()
-    {
-        $IPv6InvalidChars = '[200r::11zz]';
-        $extractedIP = HostPortExtractor::extract($IPv6InvalidChars);
-
-        $this->assertNull($extractedIP);
+        $extractedHostAndPort = HostPortExtractor::extract($input);
+        $this->assertNull($extractedHostAndPort);
     }
 }
