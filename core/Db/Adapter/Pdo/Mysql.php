cb25834cdde778ff6a929f16fa8c8b0d7c914a71
Michal Kleiner <michal@innocraft.com>
2025-01-07 15:53:38 +0100

    Remove prepared statements cache from PDO\MySQL adapter
    The cache was removed from MySQLi adapter long time ago (ref #766).

--- core/Db/Adapter/Pdo/Mysql.php
+++ core/Db/Adapter/Pdo/Mysql.php
@@ -64,13 +64,6 @@ class Mysql extends Zend_Db_Adapter_Pdo_Mysql implements AdapterInterface
         parent::__construct($config);
     }
 
-    public function closeConnection()
-    {
-        $this->cachePreparedStatement = [];
-
-        parent::closeConnection();
-    }
-
     /**
      * Returns connection handle
      *
@@ -300,40 +293,6 @@ class Mysql extends Zend_Db_Adapter_Pdo_Mysql implements AdapterInterface
             // In case of the driver doesn't support getting attributes
         }
         return null;
-    }
-
-    /**
-     * @var \Zend_Db_Statement_Pdo[]
-     */
-    private $cachePreparedStatement = array();
-
-    /**
-     * Prepares and executes an SQL statement with bound data.
-     * Caches prepared statements to avoid preparing the same query more than once
-     *
-     * @param string|Zend_Db_Select $sql The SQL statement with placeholders.
-     * @param array $bind An array of data to bind to the placeholders.
-     * @return Zend_Db_Statement_Interface
-     */
-    public function query($sql, $bind = array())
-    {
-        if (!is_string($sql)) {
-            return parent::query($sql, $bind);
-        }
-
-        if (isset($this->cachePreparedStatement[$sql])) {
-            if (!is_array($bind)) {
-                $bind = array($bind);
-            }
-
-            $stmt = $this->cachePreparedStatement[$sql];
-            $stmt->execute($bind);
-            return $stmt;
-        }
-
-        $stmt = parent::query($sql, $bind);
-        $this->cachePreparedStatement[$sql] = $stmt;
-        return $stmt;
     }
 
     /**
