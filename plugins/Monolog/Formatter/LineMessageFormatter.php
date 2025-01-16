bbda5297a09c0ebd0fbbeabcfbced3fa62a2cead
sgiehl <stefan@matomo.org>
2025-01-16 11:16:55 +0100

    Escape control characters in log messages

--- plugins/Monolog/Formatter/LineMessageFormatter.php
+++ plugins/Monolog/Formatter/LineMessageFormatter.php
@@ -53,6 +53,8 @@ class LineMessageFormatter implements FormatterInterface
         $total = '';
 
         foreach ($messages as $message) {
+            // escape control characters
+            $message = addcslashes($message, "\x00..\x1F\x7F");
             $message = $this->prefixMessageWithRequestId($record, $message);
             $total  .= $this->formatMessage($class, $message, $date, $record);
         }
